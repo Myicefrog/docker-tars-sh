10c4fd9b679e2a366d8443616034a9acc3e389e1
Myicefrog <490925498@qq.com>
2019-08-03 09:42:03 +0000

    update

--- host_run_docker_tars.sh
+++ host_run_docker_tars.sh
@@ -1,2 +1,2 @@
-sudo docker run -d -it --name host-tars  --env MOUNT_DATA=true --env DBIP=192.168.79.134 --env DBPort=3306 --env DBUser=root --env DBPassword=password -p 3000:3000 --net=host --env INET_NAME=ens33 -v /data:/data tarscloud/tars:dev
+sudo docker run -d -it --name host-tars  --env MOUNT_DATA=true --env DBIP=192.168.79.134 --env DBPort=3306 --env DBUser=root --env DBPassword=password -p 3000:3000 -v /data:/data tarscloud/tars:dev
 
