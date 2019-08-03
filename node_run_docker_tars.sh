826e50cdba7a476e33ccdcd3815eb06dcee8f0d7
Myicefrog <490925498@qq.com>
2019-08-03 12:00:10 +0000

    update

--- node_run_docker_tars.sh
+++ node_run_docker_tars.sh
@@ -1,2 +1,2 @@
-sudo docker run -d -it --name node-tars  --env MOUNT_DATA=true --env DBIP=192.168.128.139 --env DBPort=3306 --env DBUser=root --env DBPassword=password --env MASTER=192.168.128.139 -v /data:/data  zxjt/tarsnode:v1 
+sudo docker run -d -it --name node-tars  --env MOUNT_DATA=true --env DBIP=192.168.128.139 --env DBPort=3306 --env DBUser=root --env DBPassword=password --env MASTER=192.168.128.139 -v /data:/data --net=host --env INET_NAME=ens33 zxjt/tarsnode:v1 
 
