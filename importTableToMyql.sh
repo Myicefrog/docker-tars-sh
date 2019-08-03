0ec0d66d040306026304992c14f599dde660035b
Myicefrog <490925498@qq.com>
2019-08-03 10:02:32 +0000

    update

--- importTableToMyql.sh
+++ importTableToMyql.sh
@@ -1,1 +1,1 @@
-mysql -h 192.168.79.134 -P 3306 -u root -ppassword db_tars < t_server_notifys.sql
+mysql -h 192.168.128.139 -P 3306 -u root -ppassword db_tars < t_server_notifys.sql
--- node_run_docker_tars.sh
+++ node_run_docker_tars.sh
@@ -1,2 +1,2 @@
-sudo docker run -d -it --name node-tars  --env MOUNT_DATA=true --env DBIP=192.168.79.134 --env DBPort=3306 --env DBUser=root --env DBPassword=password --net=host --env INET_NAME=ens33 --env MASTER=192.168.79.134 -v /data:/data  zxjt/tarsnode:v1 
+sudo docker run -d -it --name node-tars  --env MOUNT_DATA=true --env DBIP=192.168.128.139 --env DBPort=3306 --env DBUser=root --env DBPassword=password --env MASTER=192.168.128.139 -v /data:/data  zxjt/tarsnode:v1 
 
